3ef53d42051d564628928bc753dc14179ba5587f
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-08-10 03:11:28 +0100

    projectupload

---
+++ MvcProject/MvcWebApp/MvcWebApp/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebData.Data;
+using WebData.Data.Entities;
+
+namespace MvcWebApp.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+          services.AddDbContext<WebAppContext>(opts =>
+          opts.UseMySql(configuration.GetConnectionString("SqlConnection"), new MySqlServerVersion(new Version(8, 0, 11)), b => b.MigrationsAssembly("MvcWebApp")));
+
+        public static void ConfigureIdentity(this IServiceCollection services) =>
+            services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<WebAppContext>()
+            .AddDefaultTokenProviders();
+
+    }
+}
---
+++ MvcProject/MvcWebApp/WebData/Data/WebAppContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebData.Data.Entities;
+
+namespace WebData.Data
+{
+    public class WebAppContext:IdentityDbContext<User>
+    {
+        public WebAppContext(DbContextOptions<WebAppContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);  
+        }
+    }
+}
